9adda1af31ad1adbb2542c07ccb284af67cb386b
Appolzz <benwendell09@gmail.com>
2026-04-15 10:31:30 -0400

    Square

--- Warmup3/script.js
+++ Warmup3/script.js
@@ -1,17 +1,11 @@
 
 var button = document.querySelector('.btn');
-var tri1 = document.querySelector('.tri1');
-var tri2 = document.querySelector('.tri2');
+var box = document.querySelector('.box');
 
 button.onclick = function() {
-  if (tri1.style.animationPlayState === 'paused') {
-    tri1.style.animationPlayState = 'running';
+  if (box.style.animationPlayState === 'paused') {
+    box.style.animationPlayState = 'running';
   } else {
-    tri1.style.animationPlayState = 'paused';
-  }
-  if (tri2.style.animationPlayState === 'paused') {
-    tri2.style.animationPlayState = 'running';
-  } else {
-    tri2.style.animationPlayState = 'paused';
+    box.style.animationPlayState = 'paused';
   }
 };
